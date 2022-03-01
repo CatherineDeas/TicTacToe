f3f2d2668cafd8ed7a28a78550f671bdcc817ff1
Catherine Deas <catherine.deas@madetech.com>
2022-03-01 17:13:41 +0000

    spec file

---
+++ spec/board_spec.rb
@@ -0,0 +1,15 @@
+require 'board.rb'
+
+    describe 'tictactoe' do
+        it "has a gameboard" do
+            # Arrange
+            tictactoe = TicTacToe.new
+    
+            # Act
+            result = tictactoe.gameboard([])
+    
+            # Assert
+            expect(result).to eq([])
+        end
+
+end
